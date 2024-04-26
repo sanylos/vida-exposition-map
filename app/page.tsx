02c4f2f7e28761a2356e3ad4550521534f5ad969
sanyl <homolkadam@seznam.cz>
2024-04-26 19:51:13 +0200

    Added image

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      
+      <div className="container">
+        <img alt="Mapa - první patro" src="https://mscb.vida.cz/_media/cz/prace/programator2024/czmap.png" />
+      </div>
     </main>
   );
 }
