25c2a2b8ffc09397d77181c33451840d8a810117
sanyl <homolkadam@seznam.cz>
2024-04-26 20:04:19 +0200

    Added map card

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <div className="container">
-        <img alt="Mapa - první patro" src="https://mscb.vida.cz/_media/cz/prace/programator2024/czmap.png" />
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-cyan-100">
+        <img className="w-full" src="https://mscb.vida.cz/_media/cz/prace/programator2024/czmap.png" alt="Sunset in the mountains" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl text-black mb-2">Expozice</div>
+          <p className="text-black text-base">
+            expozice 1, expozice 2, ...
+          </p>
+        </div>
       </div>
     </main>
   );
