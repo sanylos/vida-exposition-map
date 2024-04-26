d8a168f8ac95e23b5fcfa7b07e4749e08527062f
sanyl <homolkadam@seznam.cz>
2024-04-26 20:28:48 +0200

    Improved alignment

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-cyan-100">
-        <img className="w-full" src="https://mscb.vida.cz/_media/cz/prace/programator2024/czmap.png" alt="Sunset in the mountains" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl text-black mb-2">Expozice</div>
-          <p className="text-black text-base">
-            expozice 1, expozice 2, ...
-          </p>
+      
+      <div className="flex flex-row items-center justify-center container">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-cyan-100">
+          <img className="w-full" src="https://mscb.vida.cz/_media/cz/prace/programator2024/czmap.png" alt="Sunset in the mountains" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl text-black mb-2">Expozice</div>
+            <p className="text-black text-base">
+              expozice 1, expozice 2, ...
+            </p>
+          </div>
         </div>
       </div>
     </main>
