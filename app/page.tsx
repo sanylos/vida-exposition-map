806724b6794939b545732f2ddf5f16571dc405cd
sanyl <homolkadam@seznam.cz>
2024-04-27 19:05:21 +0200

    Added centering div

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ export default function Home() {
         <img className="absolute inset-0 w-full h-full object-cover" src="https://vida.cz/images/headers/main/145/_thumb1/expoziceweb-1.webp" alt="Sunset in the mountains" />
         <div className="overlay"></div>
         <div className="absolute inset-0 bg-black opacity-50"></div>
-
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white text-black relative z-10">
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Expozice</div>
-            <p className="text-base">
-              expozice 1, expozice 2, ...
-            </p>
+        <div className="flex justify-center items-center p-10">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white text-black relative z-10">
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Expozice</div>
+              <p className="text-base">
+                expozice 1, expozice 2, ...
+              </p>
+            </div>
           </div>
-
         </div>
       </div>
       <div className="flex flex-row items-center justify-center container">
