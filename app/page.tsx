dfe487c5c416f06c6cf89acd61e40e114c685b8b
sanyl <homolkadam@seznam.cz>
2024-04-27 19:10:38 +0200

    Added map display

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,12 @@ export default function Home() {
         <img className="absolute inset-0 w-full h-full object-cover" src="https://vida.cz/images/headers/main/145/_thumb1/expoziceweb-1.webp" alt="Sunset in the mountains" />
         <div className="overlay"></div>
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="flex justify-center items-center p-10">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white text-black relative z-10">
+        <div className="flex justify-center items-center p-1 md:p-10">
+          <div className="rounded overflow-hidden shadow-lg bg-white text-black relative z-10">
             <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Expozice</div>
-              <p className="text-base">
-                expozice 1, expozice 2, ...
-              </p>
+              <div className="font-bold text-xl mb-2">Mapa expozice</div>
+              <img className="w-auto h-auto" src="https://mscb.vida.cz/_media/cz/prace/programator2024/czmap.png" alt="Mapa expozice" />
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-row items-center justify-center container">
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-cyan-100">
-          <img className="w-full" src="https://mscb.vida.cz/_media/cz/prace/programator2024/czmap.png" alt="Sunset in the mountains" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl text-black mb-2">Expozice</div>
-            <p className="text-black text-base">
-              expozice 1, expozice 2, ...
-            </p>
           </div>
         </div>
       </div>
