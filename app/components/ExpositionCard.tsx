67fa9861dd2974592e7ce353adab03c1d780ed2c
sanyl <homolkadam@seznam.cz>
2024-04-28 21:02:50 +0200

    Added card height

--- app/components/ExpositionCard.tsx
+++ app/components/ExpositionCard.tsx
@@ -12,7 +12,7 @@ const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
             backgroundImage: 'url(' + exposition.image + ')',
             transition: 'ease 2s background-position'
         }}
-            className="bg-cover bg-start hover:bg-center mb-1 flex flex-col items-center justify-between bg-white rounded-lg shadow md:flex-row md:max-w-xl">
+            className="h-48 bg-cover bg-start hover:bg-center mb-1 flex flex-col items-center justify-between bg-white rounded-lg shadow md:flex-row md:max-w-xl">
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-200">{exposition.name}</h5>
                 <p className="mb-3 font-normal text-gray-100">{exposition.description.slice(0, 64)}...</p>
