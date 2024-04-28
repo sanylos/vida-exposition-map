41d2d1d8bb64977fa7348d6b36fc477eec1d0bf5
sanyl <homolkadam@seznam.cz>
2024-04-28 21:06:42 +0200

    Updated colors

--- app/components/ExpositionCard.tsx
+++ app/components/ExpositionCard.tsx
@@ -14,10 +14,10 @@ const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
         }}
             className="h-48 bg-cover bg-start hover:bg-center mb-1 flex flex-col items-center justify-between bg-white rounded-lg shadow md:flex-row md:max-w-xl">
             <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-200">{exposition.name}</h5>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-stone-200">{exposition.name}</h5>
                 <p className="mb-3 font-normal text-gray-100">{exposition.description.slice(0, 64)}...</p>
             </div>
-            <MdArrowForwardIos className="text-6xl text-gray-100 shadow-2xl me-2" />
+            <MdArrowForwardIos className="text-6xl text-stone-200 shadow-2xl me-2" />
         </div>
 
     )
