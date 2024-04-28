6c29adcb8ed346ad4343c52056f792299fdf3506
sanyl <homolkadam@seznam.cz>
2024-04-28 20:00:49 +0200

    Added breakpoints flex layout

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
         <img className="absolute inset-0 w-full h-full object-cover" src="https://vida.cz/images/headers/main/145/_thumb1/expoziceweb-1.webp" alt="Sunset in the mountains" />
         <div className="overlay"></div>
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="flex flex-row justify-center items-start p-1 md:p-10">
+        <div className="flex flex-col lg:flex-row justify-center items-start p-1 md:p-10">
           <div className="rounded overflow-hidden shadow-lg bg-white text-black relative z-10">
             <div className="px-6 py-4">
               <div className="flex justify-between items-center">
