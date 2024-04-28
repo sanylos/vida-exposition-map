2cc60d11cecf86d18820cc1847142991b59df1c9
sanyl <homolkadam@seznam.cz>
2024-04-28 20:58:12 +0200

    Added description slicing to first 64 chars

--- app/components/ExpositionCard.tsx
+++ app/components/ExpositionCard.tsx
@@ -5,19 +5,21 @@ interface Exposition {
     floor: number,
     image: string
 }
+import { MdArrowForwardIos } from "react-icons/md";
 const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
     return (
         <div style={{
             backgroundImage: 'url(' + exposition.image + ')',
             transition: 'ease 2s background-position'
         }}
-            className="bg-cover bg-start hover:bg-center mb-1 flex flex-col items-center bg-white rounded-lg shadow md:flex-row md:max-w-xl">
+            className="bg-cover bg-start hover:bg-center mb-1 flex flex-col items-center justify-between bg-white rounded-lg shadow md:flex-row md:max-w-xl">
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-200">{exposition.name}</h5>
-                <p className="mb-3 font-normal text-gray-100">{exposition.description}</p>
+                <p className="mb-3 font-normal text-gray-100">{exposition.description.slice(0, 64)}...</p>
             </div>
-
+            <MdArrowForwardIos className="text-6xl text-gray-100 shadow-2xl me-2" />
         </div>
+
     )
 }
 
