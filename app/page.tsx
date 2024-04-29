f1964fd3e4001c0195ebec217db77ffd1aaf8b07
sanyl <homolkadam@seznam.cz>
2024-04-29 19:57:50 +0200

    Updated exposition list height

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
               <Image width={1500} height={960} src="/images/expozice_mapa1.png" alt="Mapa expozice" />
             </div>
           </div>
-          <div className="w-full lg:w-1/3 rounded overflow-hidden shadow-lg text-black relative z-10 h-full">
+          <div className="w-full lg:w-1/3 rounded overflow-hidden shadow-lg text-black relative z-10 h-screen lg:overflow-y-auto">
             {expositions.map((exposition, index) => (
               <div key={index}>
                 <ExpositionCard exposition={exposition} />
