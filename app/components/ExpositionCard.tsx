2fa8d4f3ae642cb6ffb77332d61b376b4de100e6
sanyl <homolkadam@seznam.cz>
2024-04-30 20:26:12 +0200

    Added bottom margin for large breakpoint

--- app/components/ExpositionCard.tsx
+++ app/components/ExpositionCard.tsx
@@ -12,7 +12,7 @@ const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
             backgroundImage: 'url(' + exposition.image + ')',
             transition: 'ease 2s background-position'
         }}
-            className="my-1 lg:my-0 lg:mx-1 h-48 w-full bg-cover bg-start hover:bg-center flex flex-col items-center justify-between bg-white rounded-lg shadow md:flex-row">
+            className="my-1 lg:my-0 lg:mb-1 lg:mx-1 h-48 w-full bg-cover bg-start hover:bg-center flex flex-col items-center justify-between bg-white rounded-lg shadow md:flex-row">
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-stone-200">{exposition.name}</h5>
                 <p className="mb-3 font-normal text-gray-100">{exposition.description.slice(0, 64)}...</p>
