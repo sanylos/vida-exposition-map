4477dda6f0934dcfed7f699d346923a361c56258
sanyl <homolkadam@seznam.cz>
2024-04-30 20:31:30 +0200

    Added exposition filtering based on selected floor

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,9 @@ export default function Home() {
         <img className="absolute inset-0 w-full h-full object-cover" src="https://vida.cz/images/headers/main/145/_thumb1/expoziceweb-1.webp" alt="Sunset in the mountains" />
         <div className="overlay"></div>
         <div className="absolute inset-0 bg-black opacity-50"></div>
+
         <div className="flex flex-col lg:flex-row justify-center items-start p-1 md:p-10">
+
           <div className="rounded overflow-hidden shadow-lg bg-white text-black relative z-10">
             <div className="px-6 py-4">
               <div className="flex justify-between items-center">
@@ -83,8 +85,9 @@ export default function Home() {
               <Image width={1500} height={960} src={floors.find(floor => floor.number === selectedFloor)?.map_url || ''} alt="Mapa expozice" />
             </div>
           </div>
+
           <div className="w-full lg:w-1/3 rounded overflow-hidden shadow-lg text-black relative z-10 h-screen lg:overflow-y-auto">
-            {expositions.map((exposition, index) => (
+            {expositions.filter(exposition => exposition.floor == selectedFloor).map((exposition, index) => (
               <div key={index}>
                 <ExpositionCard exposition={exposition} />
               </div>
