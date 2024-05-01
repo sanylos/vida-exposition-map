4bcd92e952778442c9cdfd9421fb520ca1709b5c
sanyl <homolkadam@seznam.cz>
2024-05-01 19:36:58 +0200

    Added exhibits button

--- app/components/ExpositionCard.tsx
+++ app/components/ExpositionCard.tsx
@@ -17,7 +17,12 @@ const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-stone-200">{exposition.name}</h5>
                 <p className="mb-3 font-normal text-gray-100">{exposition.description.slice(0, 64)}...</p>
             </div>
-            <MdArrowForwardIos className="text-6xl text-stone-200 shadow-2xl me-2" />
+            <div className="flex flex-col h-full justify-end">
+                <button type="button" className="flex items-center justify-between text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                    Exponáty
+                    <MdArrowForwardIos className="ms-1 text-md" />
+                </button>
+            </div>
         </div>
 
     )
