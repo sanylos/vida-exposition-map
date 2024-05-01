74bd4010559a05b370e2c1ee3673360fabfe6db9
sanyl <homolkadam@seznam.cz>
2024-05-01 20:03:31 +0200

    Added exposition paths

--- app/page.tsx
+++ app/page.tsx
@@ -19,55 +19,64 @@ export default function Home() {
       name: 'Pár exponátů',
       description: 'Popisek pro pár exponátů',
       floor: 2,
-      image: ''
+      image: '',
+      path: '/par-exponatu'
     },
     {
       name: 'Dočasná expozice',
       description: 'Popisek pro dočasnou expozici',
       floor: 2,
-      image: ''
+      image: '',
+      path: '/docasna-expozice'
     },
     {
       name: 'Bufet',
       description: 'Popisek pro bufet',
       floor: 2,
-      image: ''
+      image: '',
+      path: '/bufet'
     },
     {
       name: 'Planeta',
       description: 'Nejdřív rozpoutejte tornádo a pokuste se ustát zemětřesení, pak si v rotující místnosti užijte účinky odstředivé síly a pohrajte si s oceánem v lahvi.',
       floor: 1,
-      image: 'https://vida.cz/images/headers/main/132/_thumb1/expimgplaneta.png'
+      image: 'https://vida.cz/images/headers/main/132/_thumb1/expimgplaneta.png',
+      path: '/planeta'
     },
     {
       name: 'Člověk',
       description: 'V obřím srdci se zaposlouchejte do vlastního tepu, vyzkoušejte si, jak namáhavá je činnost tlustého střeva, pokuste se masáží srdce zachránit lidský život, změřte si, jak vysoko vyskočíte a jaký máte postřeh, pronikněte do tajů znakové řeči, zkuste, jak vidí lidé se zrakovými vadami, vyndejte ježka z klece a vyřešte další zapeklité šifry a hlavolamy.',
       floor: 1,
-      image: 'https://vida.cz/images/headers/main/131/_thumb1/expimgclovek.png'
+      image: 'https://vida.cz/images/headers/main/131/_thumb1/expimgclovek.png',
+      path: '/clovek'
     },
     {
       name: 'Mikrosvět',
       description: 'Postavte si magnetickou sochu, užijte si pohled na elektrický oblouk, který stoupá po žebříku a prozkoumejte svět pod mikroskopem.',
       floor: 1,
-      image: 'https://vida.cz/images/headers/main/133/_thumb1/expimgmikrosvet.png'
+      image: 'https://vida.cz/images/headers/main/133/_thumb1/expimgmikrosvet.png',
+      path: '/mikrosvet'
     },
     {
       name: 'Civilizace',
       description: 'Rozjeďte ledničku na lidský pohon, postavte z lega vlastní střed Brna, napište svá přání na zeď naděje a v hlukové místnosti zkuste překřičet tryskáč.',
       floor: 1,
-      image: 'https://vida.cz/images/headers/main/153/_thumb1/expimgcivilizace.png'
+      image: 'https://vida.cz/images/headers/main/153/_thumb1/expimgcivilizace.png',
+      path: '/civilizace'
     },
     {
       name: 'Dětské science centrum',
       description: 'Na nejmenší návštěvníky tu čeká dopravní hřiště, kuličková dráha, archeologické pískoviště, obří piano, oblíbený tobogán i vodní hrátky s mlýnky.',
       floor: 1,
-      image: 'https://vida.cz/images/headers/main/154/_thumb1/expimgdetske-sc.png'
+      image: 'https://vida.cz/images/headers/main/154/_thumb1/expimgdetske-sc.png',
+      path: '/detske-science-centrum'
     },
     {
       name: 'Venkovní expozice',
       description: 'Hravé exponáty pod širým nebem. Těšte se na hučící kámen, tlakovou fontánu nebo akustické uši. To vše s krásným výhledem na pavilony brněnského výstaviště.',
       floor: 1,
-      image: 'https://vida.cz/images/headers/main/1454/_thumb1/venkovni-expozice.jpg'
+      image: 'https://vida.cz/images/headers/main/1454/_thumb1/venkovni-expozice.jpg',
+      path: '/venkovni-expozice'
     },
   ]
   const floors = [
