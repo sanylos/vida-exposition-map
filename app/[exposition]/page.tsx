312e8dfc78e59e767fdc145481efedf00549b68c
sanyl <homolkadam@seznam.cz>
2024-05-02 21:31:03 +0200

    Added grid of exhibits

--- app/[exposition]/page.tsx
+++ app/[exposition]/page.tsx
@@ -35,7 +35,17 @@ const ExpositionPage = ({ params }: { params: { exposition: string } }) => {
                                     <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{exposition.name}</h1>
                                 </header>
                                 <p className="lead">{exposition.description}</p>
+                                <div className="grid grid-cols-1 md:grid-cols-3">
+                                    {exhibits.map(exhibit => (
+                                        <div key={exhibit.name} className="mx-1 my-1 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+                                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                                        </div>
+                                    ))}
+                                </div>
                             </article>
+
                         </div>
                     </main>
                     :
