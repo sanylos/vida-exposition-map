f0f72c868e755c9536fb802cb9278913097bac19
sanyl <homolkadam@seznam.cz>
2024-05-02 22:12:32 +0200

    Updated interface

--- app/api/exhibits/[exhibit]/route.ts
+++ app/api/exhibits/[exhibit]/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 interface Exhibit {
-    name: string
+    id: string
 }
 export async function GET(request: NextRequest, { params }: { params: { exhibit: string } }) {
     const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/exhibits');
     let exhibits: Exhibit[] = await res.json();
-    const exhibit: Exhibit | undefined = exhibits.find(exhibit => exhibit.name == params.exhibit)
+    const exhibit: Exhibit | undefined = exhibits.find(exhibit => exhibit.id == params.exhibit)
     if (exhibit) {
         return NextResponse.json(exhibit)
     }
