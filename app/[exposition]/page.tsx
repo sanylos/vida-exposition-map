b77b814fcd93f8be77e9102d07441d374bca15c8
sanyl <homolkadam@seznam.cz>
2024-05-02 21:42:01 +0200

    Added exhibits cards

--- app/[exposition]/page.tsx
+++ app/[exposition]/page.tsx
@@ -37,10 +37,21 @@ const ExpositionPage = ({ params }: { params: { exposition: string } }) => {
                                 <p className="lead">{exposition.description}</p>
                                 <div className="grid grid-cols-1 md:grid-cols-3">
                                     {exhibits.map(exhibit => (
-                                        <div key={exhibit.name} className="mx-1 my-1 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-
-                                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                                            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                                        <div key={exhibit.name} className="mx-1 my-1 bg-gray-100 border border-gray-200 hover:bg-gray-200 rounded-lg shadow-2xl">
+                                            <a href="#">
+                                                <img className="rounded-t-lg h-auto w-full" src={exhibit.image} alt="" />
+                                            </a>
+                                            <div className="p-5">
+                                                <a href="#">
+                                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{exhibit.name}</h5>
+                                                </a>
+                                                <a href="#" className="w-full inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                                    Zobrazit exponát
+                                                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                                    </svg>
+                                                </a>
+                                            </div>
                                         </div>
                                     ))}
                                 </div>
