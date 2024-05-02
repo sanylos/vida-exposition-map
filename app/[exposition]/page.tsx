ed1bc2973688b10b0942cc95aeb81014d3ebfaa2
sanyl <homolkadam@seznam.cz>
2024-05-02 19:58:24 +0200

    Added exhibits fetch call

--- app/[exposition]/page.tsx
+++ app/[exposition]/page.tsx
@@ -9,6 +9,12 @@ const ExpositionPage = ({ params }: { params: { exposition: string } }) => {
         description?: string
     }
     const [exposition, setExposition] = useState<Exposition>({});
+    const [exhibits, setExhibits] = useState([]);
+    const fetchExhibits = async () => {
+        const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/expositions/' + params.exposition + '/exhibits');
+        const data = await res.json();
+        setExhibits(Object.values(data));
+    }
     const fetchExposition = async () => {
         const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/expositions/' + params.exposition);
         const data = await res.json();
@@ -16,6 +22,7 @@ const ExpositionPage = ({ params }: { params: { exposition: string } }) => {
     }
     useEffect(() => {
         fetchExposition();
+        fetchExhibits();
     }, [])
     return (
         <>
