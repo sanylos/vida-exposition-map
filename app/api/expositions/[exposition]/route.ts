c1e0c43f1ffb47fa25f8b65af8d5ad49bb84d998
sanyl <homolkadam@seznam.cz>
2024-05-02 18:52:53 +0200

    Added route

---
+++ app/api/expositions/[exposition]/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+interface exposition {
+    id: string
+}
+export async function GET(request: NextRequest, { params }: { params: { exposition: string } }) {
+    const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/expositions');
+    let expositions: exposition[] = await res.json();
+    expositions = Object.values(expositions);
+    const filteredExposition: exposition | undefined = expositions.find(exposition => exposition.id == params.exposition)
+    return NextResponse.json(filteredExposition)
+}
