53f61cd6c43b9fe31297dc00eea5ab8f9078782d
sanyl <homolkadam@seznam.cz>
2024-05-03 20:26:28 +0200

    Added button animation

--- app/components/ExpositionCard.tsx
+++ app/components/ExpositionCard.tsx
@@ -2,6 +2,7 @@
 import { Exposition } from "../page";
 import Link from "next/link";
 import { MdArrowForwardIos } from "react-icons/md";
+import { motion } from "framer-motion";
 const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
     return (
         <div style={{
@@ -15,10 +16,14 @@ const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
             </div>
             <div className="flex flex-col h-full justify-end">
                 <Link href={'/' + exposition.id}>
-                    <button type="button" className="flex items-center justify-between text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                    <motion.button type="button" className="flex items-center justify-between text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        transition={{ duration: 0.3, delay: 1, type: "spring" }}
+                    >
                         Exponáty
                         <MdArrowForwardIos className="ms-1 text-md" />
-                    </button>
+                    </motion.button>
                 </Link>
             </div>
         </div>
