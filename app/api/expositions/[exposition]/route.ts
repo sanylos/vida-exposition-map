00e39537136bf9de6c86e56245833052529f9d9e
sanyl <homolkadam@seznam.cz>
2024-05-03 19:23:29 +0200

    Imported TS interface

--- app/api/expositions/[exposition]/route.ts
+++ app/api/expositions/[exposition]/route.ts
@@ -1,13 +1,11 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
-interface exposition {
-    id: string
-}
+import { Exposition } from '@/app/page';
 export async function GET(request: NextRequest, { params }: { params: { exposition: string } }) {
     const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/expositions');
-    let expositions: exposition[] = await res.json();
+    let expositions: Exposition[] = await res.json();
     expositions = Object.values(expositions);
-    const filteredExposition: exposition | undefined = expositions.find(exposition => exposition.id == params.exposition)
+    const filteredExposition: Exposition | undefined = expositions.find(exposition => exposition.id == params.exposition)
     if (filteredExposition) {
         return NextResponse.json(filteredExposition);
     }
