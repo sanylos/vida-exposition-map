3d759805cc1b8ad152722ab79d57c804e41ec9a2
sanyl <homolkadam@seznam.cz>
2024-05-03 20:30:38 +0200

    Updated classes

--- app/components/ExpositionCard.tsx
+++ app/components/ExpositionCard.tsx
@@ -16,7 +16,7 @@ const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
             </div>
             <div className="flex flex-col h-full justify-end">
                 <Link href={'/' + exposition.id}>
-                    <motion.button type="button" className="flex items-center justify-between text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                    <motion.button type="button" className="flex items-center justify-between text-white hover:bg-gradient-to-br bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 shadow-lg shadow-teal-500/50 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                         initial={{ scale: 0 }}
                         animate={{ scale: 1 }}
                         transition={{ duration: 0.3, delay: 1, type: "spring" }}
