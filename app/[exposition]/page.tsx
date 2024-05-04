3a370318ffc6a99bfcce55e867454e57e6c1cc93
sanyl <homolkadam@seznam.cz>
2024-05-04 20:05:28 +0200

    Updated max width

--- app/[exposition]/page.tsx
+++ app/[exposition]/page.tsx
@@ -37,7 +37,7 @@ const ExpositionPage = ({ params }: { params: { exposition: string } }) => {
                 exposition ?
                     <main className="pt-5 pb-16 lg:pb-24 bg-white antialiased">
                         <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
-                            <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue">
+                            <article className="mx-auto w-full format format-sm sm:format-base lg:format-lg format-blue">
                                 <header className="mb-4 lg:mb-6 not-format">
                                     <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{exposition.name}</h1>
                                 </header>
