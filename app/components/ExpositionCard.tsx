d2c8ebd1e74bc91919b90766bb8793a14b1b81cb
sanyl <homolkadam@seznam.cz>
2024-05-04 19:42:59 +0200

    Updated breakpoints

--- app/components/ExpositionCard.tsx
+++ app/components/ExpositionCard.tsx
@@ -9,7 +9,7 @@ const ExpositionCard = ({ exposition }: { exposition: Exposition }) => {
             backgroundImage: 'url(' + exposition.image + ')',
             transition: 'ease 2s background-position'
         }}
-            className="my-1 lg:my-0 lg:mb-1 lg:mx-1 h-48 w-full bg-cover bg-start hover:bg-center flex flex-row items-center justify-between bg-white rounded-lg shadow">
+            className="my-1 2xl:my-0 h-3 md:h-80 2xl:h-52 2xl:mb-1 2xl:mx-1 h-48 w-full bg-cover bg-start hover:bg-center flex flex-row items-center justify-between bg-white rounded-lg shadow">
             <div className="h-full bg-gradient-to-r from-blue-500 to-transparent rounded flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-stone-200">{exposition.name}</h5>
                 <p className="mb-3 font-normal text-gray-100">{exposition.description.slice(0, 64)}...</p>
